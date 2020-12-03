8115a95522cdc30112b298ff264db05b8b801dc3
aarthiganesh55 <61809010+aarthiganesh55@users.noreply.github.com>
2020-12-03 17:26:38 -0500

    updates

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// const API_URL = 'https://japerk-text-processing.p.rapidapi.com/sentiment/';
+
+// // RapidAPI request headers
+// const REQUEST_HEADERS = {
+//   'X-RapidAPI-Host': 'japerk-text-processing.p.rapidapi.com'
+//   , 'X-RapidAPI-Key': '7xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+//   , 'Content-Type': 'application/x-www-form-urlencoded'
+// };
